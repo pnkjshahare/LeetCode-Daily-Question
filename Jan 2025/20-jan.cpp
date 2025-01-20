739b89ea8ca62d58f336a837977ed8ca8fdaeea6
Pankajshahare <pankajsahare.04@gmail.com>
2025-01-20 22:02:27 +0530

    20-jan

---
+++ Jan 2025/20-jan.cpp
@@ -0,0 +1,59 @@
+// Problem No :2661
+// Problem Name :First Completely Painted Row or Column
+// Solution :
+
+// brute Force :
+class Solution
+{
+public:
+    bool checkRow(int col, vector<vector<int>> &mat)
+    {
+        for (int i = 0; i < mat.size(); i++)
+        {
+            if (mat[i][col] != -1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkCol(int row, vector<vector<int>> &mat)
+    {
+        for (int i = 0; i < mat[0].size(); i++)
+        {
+            if (mat[row][i] != -1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int firstCompleteIndex(vector<int> &arr, vector<vector<int>> &mat)
+    {
+        int rowSize = mat.size();
+        int colSize = mat[0].size();
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int paintRow;
+            int paintCol;
+            for (int row = 0; row < rowSize; row++)
+            {
+                for (int col = 0; col < colSize; col++)
+                {
+                    if (mat[row][col] == arr[i])
+                    {
+                        mat[row][col] = -1;
+                        paintRow = row;
+                        paintCol = col;
+                        break;
+                    }
+                }
+            }
+            if (checkRow(paintCol, mat) || checkCol(paintRow, mat))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
